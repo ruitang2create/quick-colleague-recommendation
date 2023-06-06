b678e9e04313ab460d46636f0abdd911864cbcae
Rui Tang <trtdytr93629@gmail.com>
2023-06-05 23:09:36 -0700

    feat: update the style of colleague information table in the Review component

--- src/components/RecommendationGenerator/Review.tsx
+++ src/components/RecommendationGenerator/Review.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReviewProps } from "../types";
+import { Colleague, ReviewProps } from "../types";
 
 export default function Review({
   header,
@@ -8,13 +8,31 @@ export default function Review({
 }: ReviewProps): JSX.Element {
   return (
     <div>
-      <div>
-        <div>Colleague Information:</div>
+      <div className="bg-stone-200 px-6 py-4 mb-4">
+        <div className="text-lg font-bold mb-4">Colleague Information:</div>
         {Object.keys(colleague).map((key) => {
+          const head = key.charAt(0).toUpperCase() + key.slice(1);
+          const value = colleague[key as keyof Colleague];
+          console.log(`type of the value of [${key}]: ${Array.isArray(value)}`);
+          let valueElement: JSX.Element;
+          if (!Array.isArray(value)) {
+            valueElement = <div>{value}</div>;
+          } else {
+            valueElement = (
+              <div className="flex flex-wrap">
+                {value.map((v, i) => (
+                  <div key={v}>
+                    {v}
+                    {i === value.length - 1 ? null : <span>,&nbsp;</span>}
+                  </div>
+                ))}
+              </div>
+            );
+          }
           return (
-            <div>
-              <div>{key}</div>
-              <div>{colleague[key]}</div>
+            <div className="my-2" key={key}>
+              <div className="border-b border-b-stone-500">{head}</div>
+              {valueElement}
             </div>
           );
         })}
